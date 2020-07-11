4e3b74e83fae473a9149868a63cb5448756b099b
deleonchen <root@vs-centos-02.dlc-acc.cloud>
2020-07-11 03:12:06 -0500

    Edit settings.js

--- settings.js
+++ settings.js
@@ -18,6 +18,8 @@
 // to make it available:
 //var fs = require("fs");
 
+// hello
+
 module.exports = {
     // the tcp port that the Node-RED web server is listening on
     uiPort: process.env.PORT || 1880,
